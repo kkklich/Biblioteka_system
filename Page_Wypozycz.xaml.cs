5614d2ebbda78239c7421b6b400be170f2e14f46
kkklich <32643161+kkklich@users.noreply.github.com>
2019-02-17 21:40:20 +0100

    Wypozyczanie

--- Page_Wypozycz.xaml.cs
+++ Page_Wypozycz.xaml.cs
@@ -20,12 +20,37 @@ namespace Biblioteka_system
     /// <summary>
     /// Logika interakcji dla klasy Page_Wypozycz.xaml
     /// </summary>
+    /// 
+
+
+    public class wypozyczenia
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string ksiazka { get; set; }
+
+    }
+
     public partial class Page_Wypozycz : Page
     {
         SqlConnection conn;
         Klasa_glowna wys_liste;
         string nazwa_tabeli_ksiazki;
         string nazwa_tabeli_klient;
+        DataSet ds;
+        SqlDataAdapter sqladapter;
+        DataTable tabelaZam;
+            string tytul_ksiazki;
+        string id_ksiazki;
+        string id_klienta;
+        DateTime dataoddania;
+        DateTime datadzis;
+        SqlCommandBuilder sqlbuilder;
+        bool istnienieTEMP = false;
+        DataTable TEMPtabela;
+        DataTable KlienciTable;
+        int klikniecia_zamow = 0;
+
 
         public Page_Wypozycz()
         {
@@ -65,6 +90,183 @@ namespace Biblioteka_system
 
             string polecenie_szukania_ksiazki = "select *, (imie+' '+nazwisko) as imieNazwisko from ksiazki k join autorzyKsiazki ak on ak.id_aksiazki = k.id join autor a on a.id_autor = ak.id_autor where id like '%" + dane + "%' or tytul like '%" + dane + "%' or imie like '%" + dane + "%' or nazwisko like '" + dane + "' or(imie + ' ' + nazwisko) like '%" + dane + "%' ";
             wys_liste.ShowWypozyczenia(polecenie_szukania_ksiazki, conn, nazwa_tabeli_ksiazki, Listview_Ksiazki);
+        }
+
+
+        //Przycisk Zamów
+        private void Btn_zamów_Click(object sender, RoutedEventArgs e)
+        {
+                int index_klienta = Listview_Klienci.SelectedIndex;
+                int index_ksiazki = Listview_Ksiazki.SelectedIndex;
+
+            try { 
+                if (index_klienta > -1 & index_ksiazki > -1)
+                {
+                    datadzis = DateTime.Now;
+                    dataoddania = datadzis.AddDays(30);
+
+
+
+                    KlienciTable = new DataTable();
+                    string polecenie_klienci = "select * from klienci";
+                    sqladapter = new SqlDataAdapter(polecenie_klienci, conn);
+                    sqladapter.Fill(KlienciTable);
+
+
+                    id_klienta = KlienciTable.Rows[index_klienta][0].ToString();
+
+                    string imie_klienta = KlienciTable.Rows[index_klienta][1].ToString();
+                    string nazwisko_klienta = KlienciTable.Rows[index_klienta][2].ToString();
+
+
+                    KlienciTable = new DataTable();
+                    string polecenie_ksiaki = "select *, (imie+' '+nazwisko) as imieNazwisko from ksiazki k join autorzyKsiazki ak on ak.id_aksiazki = k.id join autor a on a.id_autor = ak.id_autor ";
+                    sqladapter = new SqlDataAdapter(polecenie_ksiaki, conn);
+                    sqladapter.Fill(KlienciTable);
+                    id_ksiazki = KlienciTable.Rows[index_ksiazki]["id"].ToString();
+
+                    tytul_ksiazki = KlienciTable.Rows[index_ksiazki][1].ToString();
+
+                    Listview_zamowienia.Items.Add(new wypozyczenia() { imie = imie_klienta, nazwisko = nazwisko_klienta, ksiazka = tytul_ksiazki });
+
+
+
+
+                    TworzenieTEMP();
+                }
+            }
+            catch(SqlException )
+            {
+                TEMPtabela = new DataTable();
+                string usuwanieTEMP = "drop table TEMP";
+                sqladapter = new SqlDataAdapter(usuwanieTEMP, conn);
+                sqladapter.Fill(TEMPtabela);
+
+                istnienieTEMP = false;
+
+
+
+                TworzenieTEMP();
+
+
+
+
+                //MessageBox.Show(f.Message);
+            }
+            catch (FormatException f)
+            {
+                MessageBox.Show(f.Message);
+            }
+
+
+        }
+
+
+
+
+
+        private void TworzenieTEMP()
+        {
+
+            if (istnienieTEMP == false)
+            {
+                TEMPtabela = new DataTable();
+                string tworzenieTEMP = "create table TEMP( id_temp int primary key identity not null, id_klienta int foreign key references klienci(id), id_ksiazki int foreign key references ksiazki(id), data_wypozyczenia date not null, data_zwrotu date not null )";
+                sqladapter = new SqlDataAdapter(tworzenieTEMP, conn);
+                sqladapter.Fill(TEMPtabela);
+
+                istnienieTEMP = true;
+            }
+
+            TEMPtabela = new DataTable();
+
+            string polecenie = "select * from TEMP";
+            sqladapter = new SqlDataAdapter(polecenie, conn);
+            sqladapter.Fill(TEMPtabela);
+
+            DataRow dr = TEMPtabela.NewRow();
+
+            dr["id_klienta"] = id_klienta;
+            dr["id_ksiazki"] = id_ksiazki;
+            dr["data_wypozyczenia"] = datadzis;
+            dr["data_zwrotu"] = dataoddania;
+
+            TEMPtabela.Rows.Add(dr);
+
+            sqlbuilder = new SqlCommandBuilder(sqladapter);
+            sqladapter.Update(TEMPtabela);
+
+        }
+
+
+        private void Btn_Wypozycz_Film_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if (istnienieTEMP == true)
+                {
+
+                    int ilosc = TEMPtabela.Rows.Count;
+                    MessageBox.Show(ilosc.ToString());
+                    string dataobecna, datazwrotu;
+
+                    for (int i = 0; i < ilosc; i++)
+                    {
+
+
+
+                        id_klienta = TEMPtabela.Rows[i]["id_klienta"].ToString();
+                        id_ksiazki = TEMPtabela.Rows[i]["id_ksiazki"].ToString();
+                        dataobecna = TEMPtabela.Rows[i]["data_wypozyczenia"].ToString();
+                        datazwrotu = TEMPtabela.Rows[i]["data_zwrotu"].ToString();
+
+
+                        datadzis = DateTime.Parse(dataobecna);
+                        dataoddania = DateTime.Parse(datazwrotu);
+
+                        ds = new DataSet();
+
+                        string polecenie_wyp = "select * from wypozyczenia";
+                        sqladapter = new SqlDataAdapter(polecenie_wyp, conn);
+                        sqladapter.Fill(ds, "wypozyczenia");
+                        DataRow dr = ds.Tables["wypozyczenia"].NewRow();
+
+                        dr["id_klienta"] = id_klienta;
+                        dr["id_ksiazki"] = id_ksiazki;
+                        dr["data_wypozyczenia"] = datadzis;
+                        dr["data_zwrotu"] = dataoddania;
+
+                        ds.Tables["wypozyczenia"].Rows.Add(dr);
+
+
+
+
+                        SqlCommandBuilder sqlbuilder = new SqlCommandBuilder(sqladapter);
+                        sqladapter.Update(ds, "wypozyczenia");
+                    }
+
+
+                    TEMPtabela = new DataTable();
+                    string usuwanieTEMP = "drop table TEMP";
+                    sqladapter = new SqlDataAdapter(usuwanieTEMP, conn);
+                    sqladapter.Fill(TEMPtabela);
+
+                    istnienieTEMP = false;
+                }
+                else
+                {
+                    MessageBox.Show("Musisz wybrać zamówienie");
+                }
+
+
+            }
+            catch(SqlException f)
+            {
+                MessageBox.Show(f.Message);
+
+            }
+            
         }
     }
 }
